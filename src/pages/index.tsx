dddef5c34154e86369deb59c7ba71bfd7e28624c
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-04-08 14:40:54 -0300

    Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { styled } from './styles';
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+})
 
 export default function Home() {
   return (
-    <>
+    <Button>
       hello
-    </>
+    </Button>
   );
 }
