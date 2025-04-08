9aa57e080326040753b89932b02d70c813fe6882
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-04-08 16:49:08 -0300

    Criando carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,31 @@
 import Image from 'next/image';
 import { HomeContainer, Product } from '../styles/pages/home';
-
+import { useKeenSlider } from 'keen-slider/react'
 import camiseta1 from '../assets/1.png'
 import camiseta2 from '../assets/2.png'
 import camiseta3 from '../assets/3.png'
 
-export default function Home() {
-  return (
-    <HomeContainer>
-      <Product>
-        <Image src={camiseta1} width={520} height={480} alt='' />
+import 'keen-slider/keen-slider.min.css'
 
+export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  });
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt='' />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt='' />
 
         <footer>
@@ -25,6 +33,25 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
     </HomeContainer>
   )
 }
