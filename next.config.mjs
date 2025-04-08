6dfd60a07599e08072e90439c41d72a310332472
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-04-08 16:23:48 -0300

    Criando Estrutura da Home

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import { AppProps } from 'next/app';
 import { globalStyles } from '../styles/global';
+
 import logoImg from '../assets/logo.svg'
+
 import { Container, Header } from '../styles/pages/app';
+import Image from 'next/image';
 export default function App({ Component, pageProps }: AppProps) {
   globalStyles();
 
