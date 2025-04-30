036f9ca9ca2cf1c674962e401ca8736321368c0c
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-04-30 16:26:35 -0300

    Produto & Checkout - Estrutura da página de produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { stripe } from '../lib/stripe';
 import 'keen-slider/keen-slider.min.css'
 import { GetStaticProps } from 'next';
 import Stripe from 'stripe';
+import Link from 'next/link';
 
 interface HomeProps {
   products: {
@@ -28,16 +29,21 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product
+              className="keen-slider__slide"
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
-    </HomeContainer>
+    </HomeContainer >
   )
 }
 
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,19 @@
 import { useRouter } from 'next/router'
-import { json } from 'stream/consumers'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
     const { query } = useRouter()
     return (
-        <h1>Produtos {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Product {query.id}</h1>
+                <span>$99.99</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sit asperiores in illo, magnam sed iusto distinctio velit quam.</p>
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
